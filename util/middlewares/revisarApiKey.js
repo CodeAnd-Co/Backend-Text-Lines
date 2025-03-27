cfb2635107773510fa1b578c477876a0b20d3231
DiegoRAP2003 <diegoalfaropinto12@gmail.com>
2025-03-27 13:35:19 -0600

    arreglar errores, mover funciones a carpetas servicios

--- util/middlewares/revisarApiKey.js
+++ util/middlewares/revisarApiKey.js
@@ -1,7 +1,7 @@
 module.exports = (nombreHeader, mensajeError = "Api key invalida") => {
   return (req, res, next) => {
     const valorHeader = req.get(nombreHeader);
-    if (!valorHeader) {
+    if (valorHeader !== process.env.API_KEY) {
       return req.status(400).json({ message: mensajeError });
     }
     next();
